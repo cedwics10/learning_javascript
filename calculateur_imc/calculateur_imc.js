7e248f3b674a347c2950eaf8e7c36aebdffb19fa
cedrvn10 <brochettes40@outlook.fr>
2022-12-02 07:47:29 +0100

    Editing IMC calculator

--- calculateur_imc/calculateur_imc.js
+++ calculateur_imc/calculateur_imc.js
@@ -1,9 +1,29 @@
-function calculer_imc()
+function calculerImc()
 {
+    let situation = "";
     let poids = document.getElementById("poids").value;
     let taille =  document.getElementById("taille").value;
     let IMC = poids / taille ** 2;
-    alert(IMC);
+
+    if(IMC < 18.5)
+    {
+        situation = "Vous êtes en état de maigreur.";
+    }
+    else if(IMC < 25)
+    {
+        situation = "Votre poids est normal.";
+    }
+    else if(IMC < 30)
+    {
+        situation = "Vous êtes en surpoids";
+    }
+    else
+    {
+        situation = "Vous êtes obèse";
+    }
+
+
+    document.getElementById("imc").innerHTML = "Votre IMC est " + IMC + ". " + situation;
 }
 
 
