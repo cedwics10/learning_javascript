14d7b1f1f800fb7d14194b6eafb094d1aaa50fc8
cedrvn10 <brochettes40@outlook.fr>
2022-12-10 23:37:02 +0100

    edit calculator

--- js_calculator/script.js
+++ js_calculator/script.js
@@ -1,107 +1,90 @@
-dot = '.';
+specialSymbols = ['-', 'x', '+', '%', ''];
 
-calcExpression = [];
-textExpression = '';
-displayedResult = '0';
-calcResult = 0;
+dot = '.'
 
-function isNumber(n)
-{
-    return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+lastkeyCalcExpression = 0
+calcExpression = []
+textExpression = ''
+displayedResult = '0'
+calcResult = 0
+
+function isNumber (n) {
+  return !isNaN(parseFloat(n)) && !isNaN(n - 0)
 }
 
-function isNotNumber(n)
-{
-    return !isNumber(n);
+function isNotNumber (n) {
+  return !isNumber(n)
 }
 
-function resetCalculator()
-{
-    document.getElementById('calc-operation').innerText = '0';
-    document.getElementById('calc-typed').innerText = '0';
-    
-    calcExpression = [];
+function resetCalculator () {
+  document.getElementById('calc-operation').innerText = '0'
+  document.getElementById('calc-typed').innerText = '0'
 
-    console.log('reinit calculator');
+  lastkeyCalcExpression = 0
+  calcExpression = []
+  textExpression = ''
+  displayedResult = '0'
+  calcResult = 0
+
+  console.log('reinit calculator')
 }
 
 // check if a new symbol is authorized
-function isLogicalCompute(new_symbol)
-{
-    let forbidden_double = ['+', '-', '/'];
-    if(calcExpression.slice(-1) == new_symbol 
-    && forbidden_double.indexOf(new_symbol))
-        return false;
-    if(calcExpression.slice(-1) == new_symbol 
-    && forbidden_double.indexOf(new_symbol))
-        return false;
-    return true;
+function isLogicalCompute (new_symbol) {
+  let forbidden_double = ['+', '-', '/']
+  if (
+    calcExpression.slice(-1) == new_symbol &&
+    forbidden_double.indexOf(new_symbol)
+  )
+    return false
+  if (
+    calcExpression.slice(-1) == new_symbol &&
+    forbidden_double.indexOf(new_symbol)
+  )
+    return false
+  return true
 }
 
-function doNotConcatenate(symbol)
-{
-    if(isNumber(symbol) && !isNumber(calcExpression.slice(-1)))
-        return true;
-    if(!isNumber(symbol) && isNumber(calcExpression.slice(-1)))
-        return true;
-    if(symbol == dot && calcExpression.slice(-1).contains('.'))
-        return true;
-    return false;
+function doNotConcatenate (symbol) {
+  if (isNumber(symbol) && !isNumber(calcExpression.slice(-1))) return true
+  if (!isNumber(symbol) && isNumber(calcExpression.slice(-1))) return true
+  if (symbol == dot && calcExpression.slice(-1).contains('.')) return true
+  return false
 }
 
-function doNotAddSymbol(symbol)
-{
-
+function doNotAddSymbol (symbol) {
+  if (symbol == dot && calcExpression.slice(-1).contains('.')) return true
+  
 }
 
-
-function addSymbol(character)
-{
-    if(isLogicalCompute(character))
-    {
-        if(doNotAddSymbol(character))
-        {
-
-        }
-        else if(doNotConcatenate(character))
-        {
-            calcExpression.push(character);
-        }
-        else
-        {
-            calcExpression[calcExpression.length - 1] += character;
-        }
+function addSymbol (character) {
+  //  à modifier
+  if (isLogicalCompute(character)) {
+    if (doNotAddSymbol(character)) {
+    } else if (doNotConcatenate(character)) {
+      calcExpression.push(character)
+    } else {
+      calcExpression[calcExpression.length - 1] += character
     }
-    showComputation();
+  }
+  showComputation()
 }
 
-function removeSymbol()
-{
-    if(calcExpression.length != 0)
-        calcExpression.length = calcExpression.length - 1;
+function MakeResultDisplay () {
+  lastkeyCalcExpression = calcExpression.length - 1
+  if (
+    lastkeyCalcExpression > 0 &&
+    isNumber(calcExpression[lastkeyCalcExpression])
+  )
+    return calcExpression[lastkeyCalcExpression]
+  if (displayedResult !== '0') return displayedResult
+  return calcExpression.join(' ')
 }
 
-function MakeResultDisplay()
-{
-    if(calcExpression.length > 0 && isNumber(calcExpression[calcExpression.length-1]))
-    {    
-        return calcExpression[calcExpression.length-1];
-    }
-    else if(typeof obj.displayedResult !== undefined)
-    {    
-        return displayedResult;
-    }
-    else
-    {
-        return calcExpression.join(' ');;
-    }
-}
+function showComputation () {
+  console.log(calcExpression)
+  document.getElementById('calc-operation').innerHTML = calcExpression.join(' ')
 
-function showComputation()
-{
-    console.log(calcExpression);
-    document.getElementById('calc-operation').innerHTML = calcExpression.join(' ');
-    
-    displayedResult = MakeResultDisplay();
-    document.getElementById('calc-typed').innerHTML = displayedResult;
+  displayedResult = MakeResultDisplay()
+  document.getElementById('calc-typed').innerHTML = displayedResult
 }
