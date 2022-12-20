aa8effe44a62d063a9b9f3ee769a199262fc97c7
cedrvn10 <brochettes40@outlook.fr>
2022-12-20 19:31:38 +0100

    edit countdown to christmas

--- node_timer/timer.js
+++ node_timer/timer.js
@@ -1,23 +1,35 @@
 const xmasDay = 25;
 const xmasMonth = 11;
+const EventDesc = 'NOËL';
 
 console.log('starting timer...');
 
-
-
 function computeXmas(currentDTime) {
-    let christmasDay = new Date(currentDTime.getFullYear(), xmasDay, xmasMonth);
+    console.log('Nous sommes en année : '  + currentDTime.getFullYear());
+    let christmasDay = new Date(currentDTime.getFullYear(), xmasMonth, xmasDay);
     if (currentDTime.getDay() > xmasDay && currentDTime.getMonth() == xmasMonth)
         christmasDay.setFullYear(christmasDay.getFullYear() + 1)
 
-    let remainingSec = Math.floor((christmasDay.getTime() - currentDTime.getTime())/1000);
-    console.log(remainingSec + " secondes avant Noël !");
+
+    // Find the distance between now and the count down date
+    var distance = christmasDay.getTime() - currentDTime.getTime();
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
+    console.log('Il reste ' + days + " jours, " + hours + " heures, " +
+        minutes + " minutes et " + seconds + " secondes avant " + EventDesc);
 }
 
 setInterval(() => {
     console.clear();
     let currentDate = new Date();
-    let currentDTime = computeXmas(currentDate);
+    computeXmas(currentDate);
     console.log('Nous sommes le ' + currentDate.toLocaleDateString() +
         ' et il est ' + currentDate.toLocaleTimeString());
 }, 1000)
