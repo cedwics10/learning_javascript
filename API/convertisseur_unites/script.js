841113764f509ab706ff61a5dee277bf5492ea74
cedwics10 <57799856+cedwics10@users.noreply.github.com>
2023-01-04 19:06:24 +0100

    new button for converter corrected

--- API/convertisseur_unites/script.js
+++ API/convertisseur_unites/script.js
@@ -1,19 +1,11 @@
 // API des tau de changes de diverses devises
 const lienApiChange = 'https://api.exchangerate-api.com/v4/latest/USD'
 
-
-function showRateConv (data) {
-  let devise1 = document.getElementById('sel1').value
-  let devise2 = document.getElementById('sel2').value
-  
-  let montant = parseFloat(document.getElementById('oamount').value)
-
-  let taux1 = parseFloat(data.rates[devise1]);
-  let taux2 = parseFloat(data.rates[devise2]);
-  
-  let calcul = taux1 * montant / taux2;
-  document.getElementById('oamount').value = calcul;
+function reinitForm()
+{
+  document.getElementById("oamount").value = '';
 }
+
 async function returnCurrency () {
   res = await fetch(lienApiChange)
     .then(response => {
@@ -30,12 +22,20 @@ async function returnCurrency () {
     })
 }
 
+function showRateConv (data) {
+  let devise1 = document.getElementById('sel1').value
+  let devise2 = document.getElementById('sel2').value
+  
+  let montant = parseFloat(document.getElementById('oamount').value)
 
-function reinitForm()
-{
-  document.getElementById("oamount").value = '';
+  let taux1 = parseFloat(data.rates[devise1]);
+  let taux2 = parseFloat(data.rates[devise2]);
+  
+  let calcul = taux1 * montant / taux2;
+  document.getElementById('oamount').value = calcul;
 }
 
+
 let boutonConversion = document.getElementById('bouton_conv');
+boutonConversion.addEventListener('click',returnCurrency);
 console.log(boutonConversion);
-boutonConversion.addEventListener('click',showRateConv());
