029e7f751dbe9045d9cfa08adeaf66f66242822e
cedrvn10 <brochettes40@outlook.fr>
2023-01-07 13:43:02 +0100

    Calculator show results

--- Projects/js_calculator/script_result.js
+++ Projects/js_calculator/script_result.js
@@ -1,8 +1,14 @@
 lienApiMath = 'http://api.mathjs.org/v4/?expr=';
 
-async function computeResult(url = '', callback = '') {
-	let mathUrl = lienApiMath + calcExpression.join('');
-	let res = await fetch(mathUrl),
-		ret = await res.text();
-	document.getElementById('calc-typed').innerHTML = callback ? callback(ret) : ret;
+// async function computeResult(url = '', callback = '') {
+// 	let mathUrl = lienApiMath + calcExpression.join('');
+// 	let res = await fetch(mathUrl),
+// 		ret = await res.text();
+// 	document.getElementById('calc-typed').innerHTML = callback ? callback(ret) : ret;
+// }
+
+function computeResult() {
+	let strCalcExpression = calcExpression.join('');
+	let result = Function(`'use strict'; return (${strCalcExpression})`)()
+	document.getElementById('calc-typed').innerHTML = result;
 }
