2a3b41c448af99bc0b0c7c132dbf05ae4c989353
cedrvn10 <brochettes40@outlook.fr>
2023-01-07 14:19:04 +0100

    update calc style

--- Projects/js_calculator/script_result.js
+++ Projects/js_calculator/script_result.js
@@ -1,14 +1,29 @@
-lienApiMath = 'http://api.mathjs.org/v4/?expr=';
+function isFloat(val) {
+	var floatRegex = /^-?\d+(?:[.,]\d*?)?$/;
+	if (!floatRegex.test(val))
+		return false;
 
-// async function computeResult(url = '', callback = '') {
-// 	let mathUrl = lienApiMath + calcExpression.join('');
-// 	let res = await fetch(mathUrl),
-// 		ret = await res.text();
-// 	document.getElementById('calc-typed').innerHTML = callback ? callback(ret) : ret;
-// }
+	val = parseFloat(val);
+	if (isNaN(val))
+		return false;
+	return true;
+}
+
+function isInt(val) {
+	var intRegex = /^-?\d+$/;
+	if (!intRegex.test(val))
+		return false;
+
+	var intVal = parseInt(val, 10);
+	return parseFloat(val) == intVal && !isNaN(intVal);
+}
+
 
 function computeResult() {
 	let strCalcExpression = calcExpression.join('');
 	let result = Function(`'use strict'; return (${strCalcExpression})`)()
+	console.log(result);
+	if (isInt(result) || isFloat(result))
+		result = parseFloat(result).toFixed(8);
 	document.getElementById('calc-typed').innerHTML = result;
 }
