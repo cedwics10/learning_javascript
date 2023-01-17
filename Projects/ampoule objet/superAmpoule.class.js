d3f02bbaf4fc4599042057083c62975c4d69d430
cedrvn10 <brochettes40@outlook.fr>
2023-01-17 22:02:29 +0100

    modifying amoule exercise

--- Projects/ampoule objet/superAmpoule.class.js
+++ Projects/ampoule objet/superAmpoule.class.js
@@ -3,29 +3,45 @@ class superAmpoule extends Ampoule {
         super(document);
     }
 
+    // Changement de l'état d'une seule image ampoule
+    changerUnEtat(image) {
+        if (image.alt = "lampe allumée") {
+            image.etat = "allumé";
+            image.src = "lampe_on.gif";
+            image.alt = "lampe allumée";
+        } else {
+            image.src = "lampe_off.gif";
+            image.alt = "lampe éteinte";
+            image.etat = "éteint";
+        }
+    }
+
     changerEtat() {
         super.changerEtat();
-        let idImage = this.img.id;
 
-        let row = this.img.id[idImage.length - 1];
-        let col = this.img.id[idImage.length - 2];
+        // On récupère les objets td et table de l'image 
+        let td = this.img.parentNode;
+        let table = this.img.parentNode.parentNode.parentNode;
+        // Nous récupérons les coordonnées de l'image
+        let x = td.cellIndex;
+        let y = td.parentNode.rowIndex
 
-        for (let x = row - 1; x <= row + 1; x++) {
-            for (let y = col - 1; y <= col + 1; y++) {
-                let obj = document.getElementById('ampoule' + x + y);
-                if(obj)
-                {
-                    if (obj.etat == "éteint") {
-                        obj.etat = "allumé";
-                        obj.img.src = "lampe_on.gif";
-                        obj.img.alt = "lampe allumée";
-                    } else {
-                        obj.img.src = "lampe_off.gif";
-                        obj.img.alt = "lampe éteinte";
-                        obj.etat = "éteint";
-                    }
+        for (let deltaX = -1; deltaX <= 1; deltaX++) {
+            for (let deltaY = -1; deltaY <= 1; deltaY++) {
+                if (x + deltaX < 0 || y + deltaY < 0)
+                    continue;
+                let row = table.rows[x + deltaX];
+                console.log(row);
+                let cell = row.cells[0]; // problème
+                console.log(cell);
+                if (cell) {
+                    let image = cell.getElementsByTagName('img')[0];
+                    this.changerUnEtat(cell);
                 }
             }
         }
 
     }
+
+
+}
