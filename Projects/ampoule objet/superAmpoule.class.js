b2608d89b7ccc56ebd19f95349af636e48869bd1
cedrvn10 <brochettes40@outlook.fr>
2023-01-18 20:23:14 +0100

    update jeu ampoule

--- Projects/ampoule objet/superAmpoule.class.js
+++ Projects/ampoule objet/superAmpoule.class.js
@@ -17,31 +17,24 @@ class superAmpoule extends Ampoule {
     changerEtat() {
         super.changerEtat();
 
-        // On récupère les objets parents td et table de l'image 
-        let td = this.img.parentNode;
+        let tdAmpoule = this.img.parentNode;
         let table = this.img.parentNode.parentNode.parentNode;
-        // Nous récupérons les coordonnées de l'image
-        let y = td.cellIndex;
-        let x = td.parentNode.rowIndex
-        console.log('coordonnées ampoule : ', x, y);
 
-        // Nous parcourons les "voisins" candidats de l'image
+        let y = tdAmpoule.cellIndex;
+        let x = tdAmpoule.parentNode.rowIndex
+
         for (let coorX = Math.max(0, x - 1); coorX <= Math.min(2, x + 1); coorX++) {
             for (let coorY = Math.max(0, y - 1); coorY <= Math.min(2, y + 1); coorY++) {
                 let neighbor = table.rows[coorX].cells[coorY];
 
                 let deltaX = Math.abs(coorX - x);
                 let deltaY = Math.abs(coorY - y);
-
-                console.log('ampoule voisine', coorX, coorY);
-
-                // nous excluons les voisins inexistants, l'ampoule en elle-même (déjà modifiée) et les voisins sur la diagonale.
+                // Nous excluons les voisins invalides, inexistant et l'ampoule sur laquelle on a cliqué
                 if (typeof neighbor !== 'undefined'
                     && (coorX !== x || coorY !== y)
                     &&
                     ((deltaX !== 1) || (deltaY !== 1))
                 ) {
-                    console.log('édité');
                     let imageNeighbor = neighbor.getElementsByTagName('img')[0];
                     this.changerUnEtat(imageNeighbor);
                 }
