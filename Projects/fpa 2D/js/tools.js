b10cd846fb8dbe188e1cc43888ca934236fd3de9
cedrvn10 <brochettes40@outlook.fr>
2023-01-30 22:11:31 +0100

    Remove useless functions

--- Projects/fpa 2D/js/tools.js
+++ Projects/fpa 2D/js/tools.js
@@ -8,26 +8,6 @@ function vocaliser(chaine) {
     synth.speak(utterance);
 }
 
-/**
- * création d'un tableau html de nbl lignes et nbc colonnes
- * remplir chaque case avec un nombre aléatoire entre 1 et 10;
- */
-function creerTable(nbl, nbc, domparent) {
-    let t = document.createElement("table");
-    domparent.appendChild(t);
-    for (let i = 0; i < nbl; i++) {
-        let tr = document.createElement("tr");
-        t.appendChild(tr);
-        for (let j = 0; j < nbc; j++) {
-            let td = document.createElement("td");
-            tr.appendChild(td);
-        }
-    }
-
-    //retourne l'objet table
-    return t;
-}
-
 function jouerSon(lien, Qvolume = 1) {
     const audio = new Audio();
     audio.volume /= Qvolume;
