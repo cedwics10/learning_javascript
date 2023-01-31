f801914139ceed42641f9787e22cbc080ff2e0f9
cedrvn10 <brochettes40@outlook.fr>
2023-01-31 09:12:21 +0100

    few updates on FPA 2D

--- Projects/fpa 2D/js/Jeu.class.js
+++ Projects/fpa 2D/js/Jeu.class.js
@@ -1,6 +1,11 @@
+/**
+ * Classe représentant les propriétés du jeu
+ */
 class Jeu {
     constructor() {
         this.width = window.innerWidth;
         this.height = window.innerHeight;
+
+        this.highScore = 0;
     }
 }
--- Projects/fpa 2D/js/Partie.class.js
+++ Projects/fpa 2D/js/Partie.class.js
@@ -1,10 +1,5 @@
 /**
- * Jeu : le jeu observe les objets pour savoir si les objets
- * intéragissent entre eux !
- * Les objets observent le jeu pour avoir vraiment en temps réel l'impact
- * qu'ils auront sur le jeu.
- * Les trolls observent les données du jeu
- * La balle observe les données des joueurs , haaa
+ * Représente un niveau dans le jeu joué
  */
 class Partie {
     /**
@@ -13,5 +8,10 @@ class Partie {
      */
     constructor(jeu) {
         this.jeu = jeu;
+        this.niveau = 1;
+    }
+
+    generer() {
+
     }
 }
