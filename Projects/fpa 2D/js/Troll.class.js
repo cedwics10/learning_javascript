c2f58f576d5b6c6650dca53d5da3c265dd16a9b0
cedrvn10 <brochettes40@outlook.fr>
2023-01-31 19:27:58 +0100

    favicon

--- Projects/fpa 2D/js/Troll.class.js
+++ Projects/fpa 2D/js/Troll.class.js
@@ -2,14 +2,17 @@
  * Représente les trolls, qui sont des ennemis à tuer
  */
 class Troll extends AliveItem {
+
+    static nbTrolls = 0;
     /**
-     * 
-     * @param {Partie} partie 
-     * @param {int} numeroTroll 
-     */
-    constructor(partie, numeroTroll) {
-        super(partie, 'troll' + numeroTroll, 'troll',
+    * 
+    * @param {Partie} partie 
+    * @param {int} numeroTroll 
+    */
+    constructor(partie) {
+        super(partie, 'troll' + Troll.nbTrolls, 'troll',
             'images/troll.png', 'sound/troll.mp3', 'sound/mortTroll.mp3');
+        Troll.nbTrolls++;
 
         this.creerImg();
         this.animer();
